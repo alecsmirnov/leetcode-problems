6824234eb88a75d9027ab23695ca0dc2111b3cb2
Alexander Smirnov <alecsmirnov93@gmail.com>
2023-02-04 11:16:50 +0700

    Update solution for Plus One problem

--- Easy/66 - PlusOne.swift
+++ Easy/66 - PlusOne.swift
@@ -1,17 +1,17 @@
 class Solution {
     func plusOne(_ digits: [Int]) -> [Int] {
         var result = digits
-        var previousTen = 1
 
         for i in stride(from: result.count - 1, through: 0, by: -1) {
-            result[i] = (digits[i] + previousTen) % 10
-            previousTen = (digits[i] + previousTen) / 10
+            if digits[i] == 9 {
+                result[i] = 0
+            } else {
+                result[i] += 1
 
-            if previousTen == 0 {
                 return result
             }
         }
 
-        return previousTen == 1 ? [1] + result : result
+        return [1] + result
     }
 }
